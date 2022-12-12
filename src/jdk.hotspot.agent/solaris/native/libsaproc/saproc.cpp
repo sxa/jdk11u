853bc9ab66714a7eda7729976670c33edb992eec
Stewart X Addison <sxa@redhat.com>
2022-12-12 12:31:32 +0000

    patch-src_jdk.hotspot.agent_solaris_native_libsaproc_saproc.cpp

--- src/jdk.hotspot.agent/solaris/native/libsaproc/saproc.cpp
+++ src/jdk.hotspot.agent/solaris/native/libsaproc/saproc.cpp
@@ -27,7 +27,7 @@
 #include <thread_db.h>
 #include <strings.h>
 #include <limits.h>
-#include <demangle.h>
+#include <cxxabi.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -1221,8 +1221,13 @@ JNIEXPORT jstring JNICALL Java_sun_jvm_hotspot_debugger_proc_ProcDebuggerLocal_d
   CHECK_EXCEPTION_(NULL);
   char  buf[2*SYMBOL_BUF_SIZE + 1];
   jstring res = 0;
-  if (cplus_demangle((char*) ptr, buf, sizeof(buf)) != DEMANGLE_ESPACE) {
+  int   status;
+  char* result;
+  if ((result = abi::__cxa_demangle(ptr, NULL, NULL, &status)) != NULL) {
+    snprintf(buf, 2*SYMBOL_BUF_SIZE+1, "%s", result);
     res = env->NewStringUTF(buf);
+    // call c library's free
+    ::free(result);
   } else {
     res = name;
   }

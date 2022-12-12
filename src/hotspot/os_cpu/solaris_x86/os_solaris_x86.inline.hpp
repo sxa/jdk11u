a7ab97703398527770fd4997188ae2ce2e461f74
Stewart X Addison <sxa@redhat.com>
2022-12-12 12:29:50 +0000

    patch-src_hotspot_os__cpu_solaris__x86_os__solaris__x86.inline.hpp

--- src/hotspot/os_cpu/solaris_x86/os_solaris_x86.inline.hpp
+++ src/hotspot/os_cpu/solaris_x86/os_solaris_x86.inline.hpp
@@ -27,8 +27,16 @@
 
 #include "runtime/os.hpp"
 
+#ifdef _GNU_SOURCE
+inline jlong os::rdtsc() {
+  uint64_t res;
+  __asm__ __volatile__ ("rdtsc" : "=A" (res));
+  return (jlong)res;
+}
+#else
 extern "C" jlong _raw_rdtsc(); // In .il file
 
 inline jlong os::rdtsc() { return _raw_rdtsc(); }
+#endif
 
 #endif // OS_CPU_SOLARIS_X86_VM_OS_SOLARIS_X86_INLINE_HPP

ba408e632ec6696d28e9e5a5ccb2a5f8ef732749
Stewart X Addison <sxa@redhat.com>
2022-12-12 12:29:39 +0000

    patch-src_hotspot_os__cpu_solaris__x86_os__solaris__x86.cpp

--- src/hotspot/os_cpu/solaris_x86/os_solaris_x86.cpp
+++ src/hotspot/os_cpu/solaris_x86/os_solaris_x86.cpp
@@ -296,13 +296,27 @@ frame os::get_sender_for_C_frame(frame* fr) {
   return frame(fr->sender_sp(), fr->link(), fr->sender_pc());
 }
 
+#ifdef SPARC_WORKS
 extern "C" intptr_t *_get_current_sp();  // in .il file
+#else
+extern "C" intptr_t *_get_current_sp() {
+  register intptr_t *rsp __asm__ ("rsp");
+  return rsp;
+}
+#endif
 
 address os::current_stack_pointer() {
   return (address)_get_current_sp();
 }
 
+#ifdef SPARC_WORKS
 extern "C" intptr_t *_get_current_fp();  // in .il file
+#else
+extern "C" intptr_t *_get_current_fp() {
+  register intptr_t **rbp __asm__ ("rbp");
+  return (intptr_t*) *rbp;
+}
+#endif
 
 frame os::current_frame() {
   intptr_t* fp = _get_current_fp();  // it's inlined so want current fp

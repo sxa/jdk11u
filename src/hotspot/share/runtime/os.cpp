903f3b4699a679c7f922425282daa2153122601b
Stewart X Addison <sxa@redhat.com>
2022-12-12 12:30:41 +0000

    patch-src_hotspot_share_runtime_os.cpp

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -154,7 +154,7 @@ char* os::iso8601_time(char* buffer, size_t buffer_length, bool utc) {
   // No offset when dealing with UTC
   time_t UTC_to_local = 0;
   if (!utc) {
-#if defined(_ALLBSD_SOURCE) || defined(_GNU_SOURCE)
+#if (defined(_ALLBSD_SOURCE) || defined(_GNU_SOURCE)) && !defined(__sun)
     UTC_to_local = -(time_struct.tm_gmtoff);
 #elif defined(_WINDOWS)
     long zone;
